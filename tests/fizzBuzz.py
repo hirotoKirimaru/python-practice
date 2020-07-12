93ec448f42c268fc971682b3ddebfc4878449b6c
kirimaru <nainaistar@gmail.com>
2020-07-12 05:20:10 +0000

    [Add] fizzBuzz clear

--- tests/fizzBuzz.py
+++ tests/fizzBuzz.py
@@ -1,3 +1,10 @@
 class FizzBuzz:
     def execute(self, num):
-        return num
+        if num % 15 == 0:
+            return "FizzBuzz"
+        if num % 3 == 0:
+            return "Fizz"
+        if num % 5 == 0:
+            return "Buzz"
+        else:
+            return num
--- tests/test_fizzBuzz.py
+++ tests/test_fizzBuzz.py
@@ -7,13 +7,22 @@ def test_num1():
     actual = FizzBuzz().execute(1)
     assert actual == 1
 
+def test_num2():
+    actual = FizzBuzz().execute(2)
+    assert actual == 2
 
-# def test_1():
-#     a = 1
-#     b = 2
-#     assert a == b
+def test_num3():
+    actual = FizzBuzz().execute(3)
+    assert actual == "Fizz"
 
-# def test_2():
-#     a = 1
-#     b = 2
-#     assert a != b
+def test_num6():
+    actual = FizzBuzz().execute(6)
+    assert actual == "Fizz"
+
+def test_num10():
+    actual = FizzBuzz().execute(10)
+    assert actual == "Buzz"
+
+def test_num15():
+    actual = FizzBuzz().execute(15)
+    assert actual == "FizzBuzz"
