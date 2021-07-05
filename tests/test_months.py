09cd4cb0a53f484fc8fb5d3c73ea9030d9abcbdc
kirimaru <nainaistar@gmail.com>
2021-07-05 14:14:47 +0000

    [Add] 今月と来月

--- tests/test_months.py
+++ tests/test_months.py
@@ -3,7 +3,16 @@
 
 import datetime 
 
+now = datetime.datetime(2020, 2, 15, 20, 29, 39)
+
 # 今月の1日
-def test_datetime():
-    now = datetime.datetime(2020, 2, 15, 20, 29, 39)
+def test_now_month():
     assert now.replace(day=1) == datetime.datetime(2020,2,1,20,29,39)
+
+# 先月の1日
+def test_last_month():
+    assert (now.replace(day=1) - datetime.timedelta(days=1)).replace(day=1) == datetime.datetime(2020,1,1,20,29,39)
+
+
+
+
