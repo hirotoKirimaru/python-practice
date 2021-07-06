8054cbf7d595483946c5aa1ea5d39501fc906375
kirimaru <nainaistar@gmail.com>
2021-07-06 14:04:11 +0000

    [Add]  月末

--- tests/test_months.py
+++ tests/test_months.py
@@ -12,6 +12,12 @@ now = datetime.datetime(2020, 2, 15, 20, 29, 39)
 def test_now_month():
     assert now.replace(day=1) == datetime.datetime(2020,2,1,20,29,39)
 
+# 今月の月末
+def test_now_month():    
+    end_day = calendar.monthrange(now.year, now.month)[1]
+    assert calendar.monthrange(now.year, now.month) == (5, 29)
+    assert now.replace(day=end_day) == datetime.datetime(2020,2,29,20,29,39)
+
 # 先月の1日
 def test_last_month():
     last_month = (now.replace(day=1) - datetime.timedelta(days=1)).replace(day=1)
