2fdf755b8b6eaa06cad82f3bbfec3ae3ab4a10df
kirimaru <nainaistar@gmail.com>
2021-07-12 13:59:59 +0000

    [Add] おまじない

--- tests/test_fixture.py
+++ tests/test_fixture.py
@@ -14,10 +14,8 @@ def kakikukeko():
     return 12345
 
 
-"""fixtureを使うと DIができる"""
-
-
 def test_foo(aiueo, kakikukeko):
+    '''fixtureを使うと DIができる'''
     print(aiueo)
     print(kakikukeko)
     assert aiueo == "hiragana"
