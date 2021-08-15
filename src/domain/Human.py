ca60a3b1f9d25b01cbee2b6cfdda3c6cd51d3976
kirimaru <nainaistar@gmail.com>
2021-08-15 07:17:12 +0000

    いまいちわからぬ

---
+++ src/domain/Human.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# 親ディレクトリを読み込みたかったが…。
+
+# import sys
+# sys.path.append("../../")
+
+# print(sys.path)
+
+# # from constant/Constant import 性別
+
+# class Human:
+#     def __init__(self):
+#         self = self
+
+#     def age(self):
+#         return 0
+
