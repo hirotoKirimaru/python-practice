dd73f13fa710784df44aa613f9a649b6cffeb442
kirimaru <nainaistar@gmail.com>
2021-11-20 01:12:38 +0000

    enum検証

---
+++ src/domain/Color.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from enum import Enum, unique
+
+class Color1(Enum):
+    RED = 1 
+    BLACK = 2
+    WHITE = 2
+
+class Color2(Enum):
+    RED = (1, 1)
+    BLACK = (2, 2)
+    WHITE = (3, 2)
+
+    def __new__(cls, id, value):
+        obj = object.__new__(cls)
+        obj.id = id
+        obj._value_ = value
+        return obj
+
+# @uniqueを付けたら重複を許さない
+# @unique
+class Color3(Enum):
+    RED = (1, 1)
+    BLACK = (2, 2)
+    WHITE = (3, 2)
+
+    def __new__(cls, id, value):
+        obj = object.__new__(cls)
+        obj.id = id
+        obj._value_ = value
+        return obj
---
+++ tests/test_enum.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import pytest
+from src.domain.Color import Color1, Color2
+
+def test_foo():
+    count = 1
+    for element in Color1:    
+        assert element.value == count
+        count = count + 1
+
+def test_bar():
+    for name, element in Color2.__members__.items():  
+        print("***")
+        print(name)
+        print(element)
+        # ***
+        # RED
+        # Color2.RED
+        # ***
+        # BLACK
+        # Color2.BLACK
+        # ***
+        # WHITE
+        # Color2.BLACK
