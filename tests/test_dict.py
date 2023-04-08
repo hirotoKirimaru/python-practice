4de61fb33bdb578fd15fdb315787f474f963ac3a
水上 皓登 <nainaistar@gmail.com>
2023-04-08 07:07:05 +0000

    parameterはNoneを渡す

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -10,3 +10,18 @@ def test_normal():
     print(all_key_has_value)
     assert dict == {"a": "a", "b": "b", "c": None}
     assert all_key_has_value == {"a": "a", "b": "b"}
+
+
+class DictDomain:
+    @staticmethod
+    def action(a: str = "1", b: str = "2", c: str = "3"):
+        print(a, b, c)
+
+def test_param_method():
+    dict = {"a": "a", "b": "b", "c": None}
+    all_key_has_value = {k: v for k, v in dict.items() if v is not None}
+
+    print("11111111111111")
+    # THEN
+    DictDomain.action(**dict)
+    DictDomain.action(**all_key_has_value)
